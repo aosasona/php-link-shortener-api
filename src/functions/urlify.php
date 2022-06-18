7e8f742cd11fb82a9ee7bc167b9de2d0d3fbf99a
Ayodeji <drealayodeji@icloud.com>
2022-06-18 18:58:07 +0100

    Initial

---
+++ src/functions/urlify.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+* @desc - Checks if a URL is valid
+* @param - $url - string - The URL to be checked
+* @return - bool - True if valid, false if not
+*/
+function check($url){
+
+};
---
+++ src/index.php
@@ -0,0 +1,5 @@
+<?php
+// Destructure $_POST array
+["url" => $url] = $_POST;
+
+?>
